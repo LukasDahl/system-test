d028235c6d6983cd8735c7cbbe86580154344281
Amtoft <lukas@amtoft.eu>
2021-01-18 13:01:33 +0100

    Payment should work now. Problems with JSON builder //Wassim maybe take a look.

--- src/test/java/clients/PaymentServiceClient.java
+++ src/test/java/clients/PaymentServiceClient.java
@@ -1,9 +1,11 @@
 /**
  * @author Wassim
-*/
+ */
 
 package clients;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -24,10 +26,16 @@ public class PaymentServiceClient {
 
     public ErrorModel createPayment(double amount, String token, String merchantId, String description) {
 
-        var createPaymentRequest = new CreatePaymentRequest(amount, token, merchantId, description);
+        //var createPaymentRequest = new CreatePaymentRequest(amount, token, merchantId, description);
+        JsonObject json = Json.createObjectBuilder()
+                .add("amount", amount)
+                .add("token", token)
+                .add("merchantId", merchantId)
+                .add("description", description)
+                .build();
 
         Response response = baseUrl.path("payments").request()
-                .post(Entity.entity(createPaymentRequest, MediaType.APPLICATION_JSON));
+                .post(Entity.entity(json, MediaType.APPLICATION_JSON));
 
         if (response.getStatus() == 201) {
             return null;
